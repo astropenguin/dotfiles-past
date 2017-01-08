a3cfbc8f8f3e7a44a74dafa3e3cbb24a8e134c86
Akio Taniguchi <taniguchi@ioa.s.u-tokyo.ac.jp>
2017-01-08 18:30:49 +0900

    Rename directory (brew --> homebrew)

--- homebrew/install.sh
+++ homebrew/install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# brew/install.sh
+# homebrew/install.sh
 
 set -eu
 trap log_error ERR
@@ -11,16 +11,27 @@ DIR_DOTFILES=${1}
 LOG_NAME="[dotfiles.brew]"
 source ${DIR_DOTFILES}/installer/func.sh ${LOG_NAME}
 
+# check installation
+log_info "checking installation"
+
+if ! type brew >/dev/null 2>&1; then
+    log_info "--> Homebrew is not installed"
+    log_info "finished with no installation"
+    exit 0
+else
+    log_info "--> OK"
+fi
+
 # install formulae, casks, apps with homebrew bundle
 log_info "installing formulae, casks, apps"
 
 if log_prompt "execute full installation?"; then
     log_info "--> executing full installation (basic + extra)"
-    brew bundle --file=${DIR_DOTFILES}/brew/brewfile_basic --no-upgrade
-    brew bundle --file=${DIR_DOTFILES}/brew/brewfile_extra --no-upgrade
+    brew bundle --file=${DIR_DOTFILES}/homebrew/brewfile_basic --no-upgrade
+    brew bundle --file=${DIR_DOTFILES}/homebrew/brewfile_extra --no-upgrade
 else
     log_info "--> executing minimal installation (basic only)"
-    brew bundle --file=${DIR_DOTFILES}/brew/brewfile_basic --no-upgrade
+    brew bundle --file=${DIR_DOTFILES}/homebrew/brewfile_basic --no-upgrade
 fi
 
 # successfully finished
