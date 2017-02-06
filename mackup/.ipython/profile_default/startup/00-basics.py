47de0655b70543f88f8d2582c5080951d0701a2c
Akio Taniguchi <taniguchi@ioa.s.u-tokyo.ac.jp>
2017-02-06 12:44:35 +0900

    Add several imports

--- mackup/.ipython/profile_default/startup/00-basics.py
+++ mackup/.ipython/profile_default/startup/00-basics.py
@@ -7,17 +7,19 @@ import sys
 import glob
 import json
 import time
+from collections import OrderedDict
 from datetime import datetime, timedelta
-from functools import partial
+from functools import partial, wraps
 from inspect import getargspec, stack
 
 # dependent packages
 import numpy as np
-import numpy.ma as ma
-from astropy.io import fits
-from astropy import units as u
+from numpy import ma
 from scipy import signal
 from scipy import fftpack
 from scipy import ndimage
 from scipy import optimize
 from scipy import interpolate
+from astropy import units as u
+from astropy.io import fits
+from sklearn import decomposition
