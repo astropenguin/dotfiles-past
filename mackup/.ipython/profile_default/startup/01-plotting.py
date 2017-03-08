e8091bd7ca935011c17ee8b8ce57ae8bd8768f5b
Akio Taniguchi <taniguchi@ioa.s.u-tokyo.ac.jp>
2017-03-09 00:00:14 +0900

    Update output_notebook setting

--- mackup/.ipython/profile_default/startup/01-plotting.py
+++ mackup/.ipython/profile_default/startup/01-plotting.py
@@ -8,7 +8,9 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 sns.set_palette('Set2', 8)
 plt.set_cmap('viridis')
+plt.rcParams['figure.figsize'] = (12,6)
 
+from matplotlib import colors
 
 def in_notebook():
     try:
@@ -19,8 +21,10 @@ def in_notebook():
 
 
 if in_notebook():
+    from ipywidgets import interact
     from bokeh.io import output_file, output_notebook, push_notebook
     from bokeh.layouts import column, gridplot, row
+    from bokeh.resources import INLINE
     from bokeh.plotting import figure, save
 
     def show(obj=None, **kwargs):
@@ -34,5 +38,5 @@ if in_notebook():
         if type(obj) == plt.Figure:
             obj = to_bokeh(obj)
 
-        output_notebook(hide_banner=True)
+        output_notebook(INLINE, hide_banner=True)
         show(obj, **kwargs)
