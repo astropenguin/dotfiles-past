53cb1e7220d59c0dd9a26080614eaccf2f134d57
Akio Taniguchi <taniguchi@ioa.s.u-tokyo.ac.jp>
2017-07-21 01:09:51 +0900

    Update ipython config

--- mackup/.ipython/profile_default/ipython_config.py
+++ mackup/.ipython/profile_default/ipython_config.py
@@ -8,6 +8,3 @@ c.StoreMagics.autorestore = True
 # autoreload
 c.InteractiveShellApp.extensions = ['autoreload']
 c.InteractiveShellApp.exec_lines = ['%autoreload 2']
-
-# print function
-c.InteractiveShellApp.exec_lines = ['from __future__ import print_function']
